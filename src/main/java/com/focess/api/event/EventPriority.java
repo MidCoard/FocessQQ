3cb590906e7faf34af03d355969778e32297c3c9
MidCoard <2624646185@qq.com>
2021-09-07 17:13:59 +0800

    syntaxError

--- src/main/java/com/focess/api/event/EventPriority.java
+++ src/main/java/com/focess/api/event/EventPriority.java
@@ -3,7 +3,7 @@ package com.focess.api.event;
 public enum EventPriority {
 
     LOWEST(0), LOWER(1), LOW(2), NORMAL(3), HIGH(4), HIGHER(5), HIGHEST(6);
-`
+
     private final int priority;
 
     EventPriority(int priority) {
