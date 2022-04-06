34f438f79e526da0b071ba9373ce7519682110ed
MidCoard <2624646185@qq.com>
2022-04-06 12:51:06 +0800

    modify: make DataConverterType.java inner

---
+++ src/main/java/top/focess/qq/api/command/converter/DataConverterType.java
@@ -0,0 +1,19 @@
+package top.focess.qq.api.command.converter;
+
+
+import top.focess.command.data.DataBuffer;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Represent this field is a DataConverter
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DataConverterType {
+
+    Class<? extends DataBuffer<?>> buffer();
+}
---
+++ src/main/java/top/focess/qq/api/command/data/CommandBuffer.java
@@ -0,0 +1,61 @@
+package top.focess.qq.api.command.data;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import top.focess.command.Command;
+import top.focess.command.data.DataBuffer;
+import top.focess.command.data.StringBuffer;
+
+/**
+ * Represent a buffer of Command.
+ */
+public class CommandBuffer extends DataBuffer<Command> {
+
+    private final StringBuffer stringBuffer;
+
+    public CommandBuffer(final int size) {
+        this.stringBuffer = StringBuffer.allocate(size);
+    }
+
+    /**
+     * Allocate a CommandBuffer with fixed size
+     *
+     * @param size the target buffer size
+     * @return a CommandBuffer with fixed size
+     */
+    @NotNull
+    @Contract("_ -> new")
+    public static CommandBuffer allocate(final int size) {
+        return new CommandBuffer(size);
+    }
+
+    @Override
+    public void flip() {
+        this.stringBuffer.flip();
+    }
+
+    @Override
+    public void put(@NotNull final Command command) {
+        this.stringBuffer.put(command.getName());
+    }
+
+    @NotNull
+    @Override
+    public Command get() {
+        final String name = this.stringBuffer.get();
+        for (final Command command : Command.getCommands())
+            if (command.getName().equals(name))
+                return command;
+        throw new IllegalArgumentException("Command: " + name + " is not found");
+    }
+
+    @NotNull
+    @Override
+    public Command get(final int index) {
+        final String name = this.stringBuffer.get(index);
+        for (final Command command : Command.getCommands())
+            if (command.getName().equals(name))
+                return command;
+        throw new IllegalArgumentException("Command: " + name + " is not found");
+    }
+}
