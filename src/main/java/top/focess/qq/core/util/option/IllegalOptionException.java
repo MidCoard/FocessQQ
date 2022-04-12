43410cf38ac8ba796023c81378499327ffac245c
MidCoard <2624646185@qq.com>
2022-04-12 19:49:10 +0800

    update to 4.1.1.4000

--- src/main/java/top/focess/qq/core/util/option/IllegalOptionException.java
+++
@@ -1,7 +0,0 @@
-package top.focess.qq.core.util.option;
-
-public class IllegalOptionException extends IllegalStateException {
-    public IllegalOptionException(final String name, final String arg) {
-        super("This option " + name + " meets a wrong arg " + arg);
-    }
-}
