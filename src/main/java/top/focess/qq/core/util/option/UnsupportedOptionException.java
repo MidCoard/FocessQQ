88d81076aebd86748613ca60be4519cd3fc06619
MidCoard <2624646185@qq.com>
2022-04-12 19:59:54 +0800

    remove Option part

--- src/main/java/top/focess/qq/core/util/option/UnsupportedOptionException.java
+++
@@ -1,7 +0,0 @@
-package top.focess.qq.core.util.option;
-
-public class UnsupportedOptionException extends UnsupportedOperationException {
-    public UnsupportedOptionException(final String option) {
-        super("The option " + option + " is not supported.");
-    }
-}
