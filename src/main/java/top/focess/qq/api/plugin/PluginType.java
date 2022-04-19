ed943985eb78fa8d7aeb4c5dac741339fa0f007b
MidCoard <2624646185@qq.com>
2022-04-19 21:56:02 +0800

    remove all properties in the PluginType

--- src/main/java/top/focess/qq/api/plugin/PluginType.java
+++ src/main/java/top/focess/qq/api/plugin/PluginType.java
@@ -11,37 +11,4 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PluginType {
-
-    /**
-     * Set the dependent plugins for the plugin
-     *
-     * @return the dependent plugins or {} if there is no dependent
-     */
-    @Deprecated
-    String[] depend() default {};
-
-    /**
-     * Set the name of this plugin
-     *
-     * @return the name of the plugin
-     */
-    @Deprecated
-    String name() default "";
-
-    /**
-     * Set the author of this plugin
-     *
-     * @return the author of the plugin
-     */
-    @Deprecated
-    String author() default "MidCoard";
-
-    /**
-     * Set the version of this plugin
-     *
-     * @return the version of the plugin
-     */
-    @Deprecated
-    String version() default "1.0.0";
-
 }
