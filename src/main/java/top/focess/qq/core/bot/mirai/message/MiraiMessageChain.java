ef2aa2c06da22cab5a99d014a7393d9227d2eb4a
MidCoard <2624646185@qq.com>
2022-05-11 13:59:12 +0800

    fix MiraiMessageChain toString format error

--- src/main/java/top/focess/qq/core/bot/mirai/message/MiraiMessageChain.java
+++ src/main/java/top/focess/qq/core/bot/mirai/message/MiraiMessageChain.java
@@ -1,6 +1,7 @@
 package top.focess.qq.core.bot.mirai.message;
 
 import net.mamoe.mirai.message.data.MessageChain;
+import net.mamoe.mirai.message.data.MessageSource;
 
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -12,6 +13,6 @@ public class MiraiMessageChain extends MiraiMessage{
 
     @Override
     public String toString() {
-        return ((MessageChain)this.getMessage()).stream().map(Objects::toString).collect(Collectors.joining("\n"));
+        return ((MessageChain)this.getMessage()).stream().filter(i -> !(i instanceof MessageSource)).map(Objects::toString).collect(Collectors.joining("\n"));
     }
 }
