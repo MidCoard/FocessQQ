a69ae5dd582767e585a130625bc2081893c06004
MidCoard <2624646185@qq.com>
2024-01-07 06:12:46 +0800

    feat: add Friend#isAdministrator

--- src/main/java/top/focess/qq/api/bot/contact/Friend.java
+++ src/main/java/top/focess/qq/api/bot/contact/Friend.java
@@ -1,11 +1,14 @@
 package top.focess.qq.api.bot.contact;
 
 import org.jetbrains.annotations.NotNull;
+import top.focess.qq.FocessQQ;
+
+import java.util.Objects;
 
 /**
  * Represents a friend.
  */
-public interface Friend extends Speaker,CommandExecutor {
+public interface Friend extends Speaker, CommandExecutor {
 
     /**
      * Get the friend's raw name (its nickname)
@@ -26,4 +29,8 @@ public interface Friend extends Speaker,CommandExecutor {
      * Delete the friend
      */
     void delete();
+
+    default boolean isAdministrator() {
+        return Objects.equals(this.getId(), FocessQQ.getAdministratorId());
+    }
 }
