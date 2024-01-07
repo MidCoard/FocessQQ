f1c4a900d0f3fbce3d063b6a8f2c9e4d3ca98884
MidCoard <2624646185@qq.com>
2024-01-07 08:09:59 +0800

    feat: add permission list <permission> command

--- src/main/java/top/focess/qq/core/commands/PermissionCommand.java
+++ src/main/java/top/focess/qq/core/commands/PermissionCommand.java
@@ -14,18 +14,18 @@ import top.focess.qq.core.permission.Permission;
 import java.util.List;
 
 public class PermissionCommand extends Command {
-    
+
     public PermissionCommand() {
         super("permission");
     }
-    
+
     @Override
     public void init() {
         this.setExecutorPermission(i -> i.isAdministrator() || i.isConsole());
-        this.addExecutor((sender,data,ioHandler)->{
+        this.addExecutor((sender, data, ioHandler) -> {
             Plugin plugin = data.get(Plugin.class);
             if (plugin.getPluginDescription().getPermissions().isEmpty())
-                ioHandler.outputLang("permission-command-no-permission",plugin.getName());
+                ioHandler.outputLang("permission-command-no-permission", plugin.getName());
             else {
                 StringBuilder stringBuilder = new StringBuilder();
                 for (Permission permission : plugin.getPluginDescription().getPermissions().keySet())
@@ -34,18 +34,27 @@ public class PermissionCommand extends Command {
             }
             return CommandResult.ALLOW;
         }, CommandArgument.of("get"), CommandArgument.of(PluginDataConverter.PLUGIN_DATA_CONVERTER));
-        this.addExecutor((sender,data,ioHandler)->{
+        this.addExecutor((sender, data, ioHandler) -> {
             Plugin plugin = data.get(Plugin.class);
             Permission permission = data.get(Permission.class);
             if (plugin.getPluginDescription().addPermission(permission))
-                ioHandler.outputLang("permission-command-set-success",plugin.getName(), permission.getName());
-            else ioHandler.outputLang("permission-command-set-failed",plugin.getName(),permission.getName());
+                ioHandler.outputLang("permission-command-set-success", plugin.getName(), permission.getName());
+            else ioHandler.outputLang("permission-command-set-failed", plugin.getName(), permission.getName());
             return CommandResult.ALLOW;
         }, CommandArgument.of("set"), CommandArgument.of(PluginDataConverter.PLUGIN_DATA_CONVERTER), CommandArgument.of(PermissionDataConverter.PERMISSION_DATA_CONVERTER));
+        this.addExecutor((sender, dataCollection, ioHandler) -> {
+            Permission permission = dataCollection.get(Permission.class);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Plugin plugin : Plugin.getPlugins())
+                if (plugin.getPluginDescription().getPermissions().getOrDefault(permission, false))
+                    stringBuilder.append(' ').append(plugin.getName());
+            ioHandler.outputLang("permission-command-list-plugin", permission.getName(), stringBuilder.toString());
+            return CommandResult.ALLOW;
+        }, CommandArgument.of("list"), CommandArgument.of(PermissionDataConverter.PERMISSION_DATA_CONVERTER));
     }
 
     @Override
     public @NotNull List<String> usage(CommandSender sender) {
-        return Lists.newArrayList("Use: permission set <plugin> <permission>","Use: permission get <plugin>");
+        return Lists.newArrayList("Use: permission set <plugin> <permission>", "Use: permission get <plugin>", "Use: permission list <permission>");
     }
 }
